f616e4eb0c0900c335298632016d21c26f531b4f
jim-aban <jim.aban@student.ateneo.edu>
2026-02-16 01:09:24 +0800

    Registered models

--- BVTCApp/admin.py
+++ BVTCApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product, ProductImage, ProductColor, Order, OrderItem
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(ProductColor)
+admin.site.register(Order)
+admin.site.register(OrderItem)
