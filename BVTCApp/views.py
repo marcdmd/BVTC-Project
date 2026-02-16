ac5cf95ebc13c016e8d10e35a35231d68325a8a6
marcdmd <marc.dungo@student.ateneo.edu>
2026-02-16 09:50:29 +0800

    added 'add product' logic to catalog views

--- BVTCApp/views.py
+++ BVTCApp/views.py
@@ -1,7 +1,38 @@
-from django.shortcuts import render
-from .models import Product, ProductImage, ProductColor, Order, OrderItem, Company, CustomerAccount, ShippingDetails
+from django.shortcuts import render, redirect
+from .models import Product, ProductImage, ProductColor
 
 def catalog(request):
+    if request.method == 'POST':
+        # create product object
+        new_product = Product.objects.create(
+            product_code=request.POST.get('product-id'),
+            product_name=request.POST.get('product-name'),
+            category=request.POST.get('category'),
+            description=request.POST.get('product-description'),
+            starting_price=request.POST.get('starting-price') or 0.00,  #default to 0.00 if no input
+            MOQ=request.POST.get('moq') or 1 #default to 1 if no input
+        )
+
+        # create ProductColor objects for each color input
+        color_list = request.POST.getlist('colors[]')
+        for color_val in color_list:
+            if color_val.strip():
+                ProductColor.objects.create(
+                    product_id=new_product, 
+                    product_color=color_val
+                )
+
+        # create ProductImage objects for each
+        image_files = request.FILES.getlist('photos[]')
+        for img in image_files:
+            ProductImage.objects.create(
+                product_id=new_product, 
+                product_image=img
+            )
+
+        return redirect('catalog')
+
+    # fetch all products including admin-inputs
     products = Product.objects.all()
     return render(request, 'bvtc_app/catalog.html', {'products': products})
 
