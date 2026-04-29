839f48d2c7ae66cc2e1ab2c23c2ed2470cdb62ff
marcdmd <marc.dungo@student.ateneo.edu>
2026-04-29 21:50:12 +0800

    registered geography shi into admin. fixed dropdown in this DB

--- BVTCApp/admin.py
+++ BVTCApp/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Product, ProductImage, ProductColor, Order, OrderItem, Company, CustomerAccount, ShippingDetails, UserAccount
+from .models import (
+    Product, ProductImage, ProductColor, Order, OrderItem, 
+    Company, CustomerAccount, UserAccount, 
+    Province, City, Barangay, ShippingDetails
+)
 
 admin.site.register(UserAccount)
 admin.site.register(Product)
@@ -9,4 +13,7 @@ admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(Company)
 admin.site.register(CustomerAccount)
+admin.site.register(Province)
+admin.site.register(City)
+admin.site.register(Barangay)
 admin.site.register(ShippingDetails)
